c58d2d21665669cf75ff3742fea3bad0d7a8ba5b
NickChristod <nickchristod98@example.com>
2026-02-03 10:35:20 +0200

    site open 2md attempt

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -179,11 +179,8 @@
 
             <div class="dropdown-divider"></div>
 
-            <button class="dropdown-item" @click="store.showTrainingDialog = true; isFileMenuOpen = false">
-              <i class="pi pi-book"></i> App Tour
-            </button>
-            <button class="dropdown-item" @click="store.showInteractiveTraining = true; isFileMenuOpen = false">
-              <i class="pi pi-graduation-cap"></i> Training
+            <button class="dropdown-item dropdown-item-disabled" disabled>
+              <i class="pi pi-book"></i> Training
             </button>
             <button v-if="!props.isMobile" class="dropdown-item" @click="showShortcuts">
               <i class="pi pi-question-circle"></i> Keyboard Shortcuts (Ctrl+/)
